988e80353e7efe20f226c75365fab663f5e49287
Prateek Pandey <prateekpandey@Prateeks-MacBook-Pro.local>
2019-10-06 22:13:11 +0530

    Starting React Learning

---
+++ hackernews/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    const helloWorld = 'Welcome to React by Prateek';
+    return (
+      <div className = "App">
+        <h2>{helloWorld}</h2>
+      </div>
+    );
+  }
+}
+
+
+export default App;
